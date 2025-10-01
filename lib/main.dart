68dde9a804809978b9d5afbc5f6ac714ccfd72ac
sharko <shar40951@gmail.com>
2025-10-01 20:04:07 +0300

    `Removed statusBarIconBrightness from SystemUiOverlayStyle`

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ void main() async {
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
       statusBarColor: Colors.white,
-      
       statusBarIconBrightness: Brightness.dark,
     ),
   );
